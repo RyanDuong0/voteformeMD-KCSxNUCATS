716257207255c4390fb03e82857ba15125a647bf
Ryan <R.Duong2@newcastle.ac.uk>
2025-02-26 15:02:05 +0000

    Removed unnecessary tax dict in tax_constants.py

--- python_src/calculator.py
+++ python_src/calculator.py
@@ -1,3 +1,12 @@
+import tax_constants
+
+def get_tax_rate(country):
+    country=country.upper()
+    return tax_constants.country_tax_rates.get(country)
+
 def main():
     user_country = input("Enter country you are in: ")
     buy_country = input("Enter country you are buying from: ")
+    print(get_tax_rate("united kingdom"))
+
+main()
