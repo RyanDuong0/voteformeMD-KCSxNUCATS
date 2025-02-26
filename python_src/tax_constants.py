4f72d5fa829910746ed1facf90ba1978aa4dee6b
u02ff23 <u02ff23@abdn.ac.uk>
2025-02-26 14:49:43 +0000

    added tax rates in hash map

---
+++ python_src/tax_constants.py
@@ -0,0 +1,19 @@
+tax_rates_with_currency = {
+    "UNITED KINGDOM": {"Tax Rate": 25, "Currency": "£"},
+    "IRELAND": {"Tax Rate": 12.5, "Currency": "€"},
+    "NETHERLANDS": {"Tax Rate": 25.8, "Currency": "€"},
+    "BELGIUM": {"Tax Rate": 25, "Currency": "€"},
+    "SOUTH AFRICA": {"Tax Rate": 27, "Currency": "R"},
+    "KENYA": {"Tax Rate": 30, "Currency": "KSh"},
+    "CANADA": {
+        "Tax Rate": {"Federal": 15, "Provincial Range": (8, 16)},
+        "Currency": "C$"
+    },
+    "AUSTRALIA": {
+        "Tax Rate": {"Standard": 30, "Small-Medium Business": 25},
+        "Currency": "A$"
+    },
+    "FINLAND": {"Tax Rate": 20, "Currency": "€"},
+    "SWEDEN": {"Tax Rate": 20.6, "Currency": "kr"}
+}
+
